f15c734b1d81d52b3dfaad0d211511a977dbefa6
Saurav <sbhuju61@gmail.com>
2020-02-24 17:05:02 -0700

    added eureka

---
+++ src/test/java/com/ss/lms/TestBookDAO.java
@@ -0,0 +1,13 @@
+package com.ss.lms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ss.lms.DAO.BookDAO;
+
+public class TestBookDAO {
+	@Autowired
+	BookDAO bDAO;
+	
+	
+	
+}
