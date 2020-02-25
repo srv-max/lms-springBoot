21de0ce2892cb0e18d27d84bc3c1f0b75a1fe7fd
Saurav <sbhuju61@gmail.com>
2020-02-24 17:29:24 -0700

    added readBook test

--- src/test/java/com/ss/lms/TestBookDAO.java
+++ src/test/java/com/ss/lms/TestBookDAO.java
@@ -1,13 +1,21 @@
 package com.ss.lms;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ss.lms.DAO.BookDAO;
+import com.ss.lms.entity.Book;
 
 public class TestBookDAO {
 	@Autowired
 	BookDAO bDAO;
 	
+	public void testReadBoook() {
+		List<Book> listOfBooks = bDAO.findAll();
+		Assertions.assertEquals(false,listOfBooks.isEmpty());
+	}
 	
 	
 }
